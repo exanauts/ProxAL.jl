8c345d063298bcf47bc51478d1c4b955d7284b67
Anirudh Subramanyam <asubramanyam@moonshot.cels.anl.gov>
2020-05-26 14:07:22 -0500

    updated test scripts

--- sctest.jl
+++ sctest.jl
@@ -13,7 +13,7 @@ include("analysis.jl")
 
 ENV["GKSwstype"]="nul"
 
-for (idx, case) in enumerate(["data/case9", ARGS[1]])
+for case in [ARGS[1]]
     T = parse(Int, ARGS[2])
     scen = "data/mp_demand/"*basename(case)*"_onehour_60"
     rawdata = RawData(case, scen)
@@ -23,15 +23,32 @@ for (idx, case) in enumerate(["data/case9", ARGS[1]])
     opfdata = opf_loaddata(rawdata)
     opt = Option()
     opt.sc_constr = true
-    opt.freq_ctrl = true
+    opt.freq_ctrl = false
     opt.two_block = false
     opt.obj_penalty = false
-    opt.weight_scencost = 0#1/length(rawdata.ctgs_arr)
+    opt.weight_scencost = 1/length(rawdata.ctgs_arr)
     opt.weight_loadshed = 2500.0
     opt.weight_freqctrl = 0
-    opt.savefile = getDataFilename("", case, "proxALM", T, opt.sc_constr, true, 0)
+    ramp_scale = Float64(opt.freq_ctrl) + 0.1Float64(opt.two_block)
+    maxρ = 10.0
 
     # include base case
     T = length(rawdata.ctgs_arr) + 1
-    @time x, λ, savedata = runProxALM(opfdata, rawdata, T; options = opt, verbose_level = 2, fullmodel = false, parallel = true)
+
+    #
+    # Solve proximal ALM from cold-start
+    #
+    #=
+    begin
+        opt.savefile = getDataFilename("sc_coldstart", case, "proxALM", T, opt.sc_constr, true, ramp_scale)
+        @time x, λ, savedata = runProxALM(opfdata, rawdata, T; options = opt, verbose_level = 2, fullmodel = false, parallel = true)
+    end
+    =#
+
+    #
+    # Proximal ALM in rolling horizon using warm-start
+    #
+    begin
+        rolling_horizon_sc_proxALM(case, opfdata, rawdata, T, ramp_scale, maxρ; options = opt, parallel = true)
+    end
 end
