b94b6d7f8d3651bcd38a5c002009f7567a488404
Anirudh Subramanyam <asubramanyam@moonshot.cels.anl.gov>
2020-07-06 22:16:13 -0500

    Wrap power balance and line flows in their own functions

--- src/scacopf_model.jl
+++ src/scacopf_model.jl
@@ -1,15 +1,6 @@
 
 function opf_fullmodel(opfdata::OPFData, rawdata::RawData, T::Int; options::Option = Option(), maxρ::Float64 = 1.0, compute_Lstar::Bool = false)
 
-    # security-constrained OPF: explicitly add the "base case"
-    if options.sc_constr
-        lines_off = [opfdata.lines[l] for l in rawdata.ctgs_arr]
-        lines_off = [Line(); lines_off]
-    else
-        lines_off = [Line() for t in 1:T]
-    end
-
-
     # the model
     opfmodel = JuMP.Model(optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 5))
 
@@ -231,7 +222,9 @@ function opf_fullmodel(opfdata::OPFData, rawdata::RawData, T::Int; options::Opti
 
         # Perturbed network data for contingencies
         if options.sc_constr
-            opfdata_c = opf_loaddata(rawdata; lineOff = lines_off[t])
+            if t > 1
+                opfdata_c = opf_loaddata(rawdata; lineOff = opfdata.lines[rawdata.ctgs_arr[t - 1]])
+            end
             Pd = [buses[b].Pd for b in 1:nbus]
             Qd = [buses[b].Qd for b in 1:nbus]
         else
@@ -239,74 +232,12 @@ function opf_fullmodel(opfdata::OPFData, rawdata::RawData, T::Int; options::Opti
             Qd = opfdata.Qd[:,t]
         end
 
-
-
-        # Network data short-hands
-        lines = opfdata_c.lines
-        busIdx = opfdata_c.BusIdx
-        FromLines = opfdata_c.FromLines
-        ToLines = opfdata_c.ToLines
-        BusGeners = opfdata_c.BusGenerators
-        nline = length(lines)
-        YffR,YffI,YttR,YttI,YftR,YftI,YtfR,YtfI,YshR,YshI = computeAdmitances(lines, buses, baseMVA)
-            
-    
         # Power Balance Equations
-        for b in 1:nbus
-            #real part
-            @NLconstraint(opfmodel, 
-                ( sum( YffR[l] for l in FromLines[b]) + sum( YttR[l] for l in ToLines[b]) + YshR[b] ) * Vm[t,b]^2 
-                + sum( Vm[t,b]*Vm[t,busIdx[lines[l].to]]  *( YftR[l]*cos(Va[t,b]-Va[t,busIdx[lines[l].to]]  ) + YftI[l]*sin(Va[t,b]-Va[t,busIdx[lines[l].to]]  )) for l in FromLines[b] )  
-                + sum( Vm[t,b]*Vm[t,busIdx[lines[l].from]]*( YtfR[l]*cos(Va[t,b]-Va[t,busIdx[lines[l].from]]) + YtfI[l]*sin(Va[t,b]-Va[t,busIdx[lines[l].from]])) for l in ToLines[b]   ) 
-                - ( sum(baseMVA*Pg[t,g] for g in BusGeners[b]) - Pd[b] + sigma_P1[t,b] - sigma_P2[t,b]) / baseMVA      # Sbus part
-                ==0
-            )
-
-            #imaginary part
-            @NLconstraint(opfmodel,
-                ( sum(-YffI[l] for l in FromLines[b]) + sum(-YttI[l] for l in ToLines[b]) - YshI[b] ) * Vm[t,b]^2 
-                + sum( Vm[t,b]*Vm[t,busIdx[lines[l].to]]  *(-YftI[l]*cos(Va[t,b]-Va[t,busIdx[lines[l].to]]  ) + YftR[l]*sin(Va[t,b]-Va[t,busIdx[lines[l].to]]  )) for l in FromLines[b] )
-                + sum( Vm[t,b]*Vm[t,busIdx[lines[l].from]]*(-YtfI[l]*cos(Va[t,b]-Va[t,busIdx[lines[l].from]]) + YtfR[l]*sin(Va[t,b]-Va[t,busIdx[lines[l].from]])) for l in ToLines[b]   )
-                - ( sum(baseMVA*Qg[t,g] for g in BusGeners[b]) - Qd[b] + sigma_Q1[t,b] - sigma_Q2[t,b]) / baseMVA      #Sbus part
-                ==0
-            )
-        end
-
+        add_real_power_balance_constraints(opfmodel, opfdata_c, Pg[t,:], Pd, Vm[t,:], Va[t,:], sigma_P1[t,:], sigma_P2[t,:])
+        add_imag_power_balance_constraints(opfmodel, opfdata_c, Qg[t,:], Qd, Vm[t,:], Va[t,:], sigma_Q1[t,:], sigma_Q2[t,:])
 
         # Line flow limits
-        nlinelim=0
-        for l in 1:nline
-            if lines[l].rateA!=0 && lines[l].rateA<1.0e10
-                nlinelim += 1
-                flowmax=(lines[l].rateA/baseMVA)^2
-
-                #branch apparent power limits (from bus)
-                Yff_abs2=YffR[l]^2+YffI[l]^2; Yft_abs2=YftR[l]^2+YftI[l]^2
-                Yre=YffR[l]*YftR[l]+YffI[l]*YftI[l]; Yim=-YffR[l]*YftI[l]+YffI[l]*YftR[l]
-                @NLconstraint(opfmodel,
-                    Vm[t,busIdx[lines[l].from]]^2 *
-                    ( Yff_abs2*Vm[t,busIdx[lines[l].from]]^2 + Yft_abs2*Vm[t,busIdx[lines[l].to]]^2 
-                    + 2*Vm[t,busIdx[lines[l].from]]*Vm[t,busIdx[lines[l].to]]*(Yre*cos(Va[t,busIdx[lines[l].from]]-Va[t,busIdx[lines[l].to]])-Yim*sin(Va[t,busIdx[lines[l].from]]-Va[t,busIdx[lines[l].to]])) 
-                    ) 
-                    - flowmax
-                    - (sigma_lineFrom[t,l]/baseMVA)
-                    <=0
-                )
-  
-                #branch apparent power limits (to bus)
-                Ytf_abs2=YtfR[l]^2+YtfI[l]^2; Ytt_abs2=YttR[l]^2+YttI[l]^2
-                Yre=YtfR[l]*YttR[l]+YtfI[l]*YttI[l]; Yim=-YtfR[l]*YttI[l]+YtfI[l]*YttR[l]
-                @NLconstraint(opfmodel, 
-                    Vm[t,busIdx[lines[l].to]]^2 *
-                    ( Ytf_abs2*Vm[t,busIdx[lines[l].from]]^2 + Ytt_abs2*Vm[t,busIdx[lines[l].to]]^2
-                    + 2*Vm[t,busIdx[lines[l].from]]*Vm[t,busIdx[lines[l].to]]*(Yre*cos(Va[t,busIdx[lines[l].from]]-Va[t,busIdx[lines[l].to]])-Yim*sin(Va[t,busIdx[lines[l].from]]-Va[t,busIdx[lines[l].to]]))
-                    )
-                    - flowmax
-                    - (sigma_lineTo[t,l]/baseMVA)
-                    <=0
-                )
-            end
-        end
+        add_line_power_constraints(opfmodel, opfdata_c, Vm[t,:], Va[t,:], sigma_lineFrom[t,:], sigma_lineTo[t,:])
     end
     return opfmodel
 end
@@ -489,72 +420,12 @@ function opf_model(opfdata::OPFData, rawdata::RawData, t::Int; options::Option =
         Qd = opfdata.Qd[:,t]
     end
 
-    # Network data short-hands
-    lines = opfdata_c.lines
-    busIdx = opfdata_c.BusIdx
-    FromLines = opfdata_c.FromLines
-    ToLines = opfdata_c.ToLines
-    BusGeners = opfdata_c.BusGenerators
-    nline = length(lines)
-    YffR,YffI,YttR,YttI,YftR,YftI,YtfR,YtfI,YshR,YshI = computeAdmitances(lines, buses, baseMVA)
-        
-
     # Power Balance Equations
-    for b in 1:nbus
-        #real part
-        @NLconstraint(opfmodel, 
-            ( sum( YffR[l] for l in FromLines[b]) + sum( YttR[l] for l in ToLines[b]) + YshR[b] ) * Vm[b]^2 
-            + sum( Vm[b]*Vm[busIdx[lines[l].to]]  *( YftR[l]*cos(Va[b]-Va[busIdx[lines[l].to]]  ) + YftI[l]*sin(Va[b]-Va[busIdx[lines[l].to]]  )) for l in FromLines[b] )  
-            + sum( Vm[b]*Vm[busIdx[lines[l].from]]*( YtfR[l]*cos(Va[b]-Va[busIdx[lines[l].from]]) + YtfI[l]*sin(Va[b]-Va[busIdx[lines[l].from]])) for l in ToLines[b]   ) 
-            - ( sum(baseMVA*Pg[g] for g in BusGeners[b]) - Pd[b] + sigma_P1[b] - sigma_P2[b]) / baseMVA      # Sbus part
-            ==0
-        )
-
-        #imaginary part
-        @NLconstraint(opfmodel,
-            ( sum(-YffI[l] for l in FromLines[b]) + sum(-YttI[l] for l in ToLines[b]) - YshI[b] ) * Vm[b]^2 
-            + sum( Vm[b]*Vm[busIdx[lines[l].to]]  *(-YftI[l]*cos(Va[b]-Va[busIdx[lines[l].to]]  ) + YftR[l]*sin(Va[b]-Va[busIdx[lines[l].to]]  )) for l in FromLines[b] )
-            + sum( Vm[b]*Vm[busIdx[lines[l].from]]*(-YtfI[l]*cos(Va[b]-Va[busIdx[lines[l].from]]) + YtfR[l]*sin(Va[b]-Va[busIdx[lines[l].from]])) for l in ToLines[b]   )
-            - ( sum(baseMVA*Qg[g] for g in BusGeners[b]) - Qd[b] + sigma_Q1[b] - sigma_Q2[b]) / baseMVA      #Sbus part
-            ==0
-        )
-    end
-
+    add_real_power_balance_constraints(opfmodel, opfdata_c, Pg, Pd, Vm, Va, sigma_P1, sigma_P2)
+    add_imag_power_balance_constraints(opfmodel, opfdata_c, Qg, Qd, Vm, Va, sigma_Q1, sigma_Q2)
 
     # Line flow limits
-    nlinelim=0
-    for l in 1:nline
-        if lines[l].rateA!=0 && lines[l].rateA<1.0e10
-            nlinelim += 1
-            flowmax=(lines[l].rateA/baseMVA)^2
-
-            #branch apparent power limits (from bus)
-            Yff_abs2=YffR[l]^2+YffI[l]^2; Yft_abs2=YftR[l]^2+YftI[l]^2
-            Yre=YffR[l]*YftR[l]+YffI[l]*YftI[l]; Yim=-YffR[l]*YftI[l]+YffI[l]*YftR[l]
-            @NLconstraint(opfmodel,
-                Vm[busIdx[lines[l].from]]^2 *
-                ( Yff_abs2*Vm[busIdx[lines[l].from]]^2 + Yft_abs2*Vm[busIdx[lines[l].to]]^2 
-                + 2*Vm[busIdx[lines[l].from]]*Vm[busIdx[lines[l].to]]*(Yre*cos(Va[busIdx[lines[l].from]]-Va[busIdx[lines[l].to]])-Yim*sin(Va[busIdx[lines[l].from]]-Va[busIdx[lines[l].to]])) 
-                ) 
-                - flowmax
-                - (sigma_lineFrom[l]/baseMVA)
-                <=0
-            )
-
-            #branch apparent power limits (to bus)
-            Ytf_abs2=YtfR[l]^2+YtfI[l]^2; Ytt_abs2=YttR[l]^2+YttI[l]^2
-            Yre=YtfR[l]*YttR[l]+YtfI[l]*YttI[l]; Yim=-YtfR[l]*YttI[l]+YtfI[l]*YttR[l]
-            @NLconstraint(opfmodel, 
-                Vm[busIdx[lines[l].to]]^2 *
-                ( Ytf_abs2*Vm[busIdx[lines[l].from]]^2 + Ytt_abs2*Vm[busIdx[lines[l].to]]^2
-                + 2*Vm[busIdx[lines[l].from]]*Vm[busIdx[lines[l].to]]*(Yre*cos(Va[busIdx[lines[l].from]]-Va[busIdx[lines[l].to]])-Yim*sin(Va[busIdx[lines[l].from]]-Va[busIdx[lines[l].to]]))
-                )
-                - flowmax
-                - (sigma_lineTo[l]/baseMVA)
-                <=0
-            )
-        end
-    end
+    add_line_power_constraints(opfmodel, opfdata_c, Vm, Va, sigma_lineFrom, sigma_lineTo)
 
     return opfmodel
 end
@@ -907,4 +778,99 @@ function computeLyapunovFunction(x::mpPrimalSolution, dual::mpDualSolution, xpre
     end
 
     return primalcost, penalty, (0.5*proximal)
+end
+
+function add_line_power_constraints(opfmodel::JuMP.Model, opfdata::OPFData, Vm, Va, sigma_lineFrom, sigma_lineTo)
+    # Network data short-hands
+    baseMVA = opfdata.baseMVA
+    buses = opfdata.buses
+    lines = opfdata.lines
+    busIdx = opfdata.BusIdx
+    YffR,YffI,YttR,YttI,YftR,YftI,YtfR,YtfI,YshR,YshI = computeAdmitances(lines, buses, baseMVA)
+
+    for l in 1:length(lines)
+        line = lines[l]
+        from = busIdx[line.from]
+        to = busIdx[line.to]
+        if line.rateA ≈ 0 || line.rateA >= 1.0e10
+            continue
+        end
+        flowmax=(line.rateA/baseMVA)^2
+
+        #branch apparent power limits (from bus)
+        Yff_abs2=YffR[l]^2+YffI[l]^2; Yft_abs2=YftR[l]^2+YftI[l]^2
+        Yre=YffR[l]*YftR[l]+YffI[l]*YftI[l]; Yim=-YffR[l]*YftI[l]+YffI[l]*YftR[l]
+        @NLconstraint(opfmodel,
+            Vm[from]^2 *
+            ( Yff_abs2*Vm[from]^2 + Yft_abs2*Vm[to]^2 
+            + 2*Vm[from]*Vm[to]*(Yre*cos(Va[from]-Va[to])-Yim*sin(Va[from]-Va[to])) 
+            ) 
+            - flowmax
+            - (sigma_lineFrom[l]/baseMVA)
+            <=0
+        )
+
+        #branch apparent power limits (to bus)
+        Ytf_abs2=YtfR[l]^2+YtfI[l]^2; Ytt_abs2=YttR[l]^2+YttI[l]^2
+        Yre=YtfR[l]*YttR[l]+YtfI[l]*YttI[l]; Yim=-YtfR[l]*YttI[l]+YtfI[l]*YttR[l]
+        @NLconstraint(opfmodel, 
+            Vm[to]^2 *
+            ( Ytf_abs2*Vm[from]^2 + Ytt_abs2*Vm[to]^2
+            + 2*Vm[from]*Vm[to]*(Yre*cos(Va[from]-Va[to])-Yim*sin(Va[from]-Va[to]))
+            )
+            - flowmax
+            - (sigma_lineTo[l]/baseMVA)
+            <=0
+        )
+    end
+end
+
+function add_real_power_balance_constraints(opfmodel::JuMP.Model, opfdata::OPFData, Pg, Pd, Vm, Va, sigma_P1, sigma_P2)
+    # Network data short-hands
+    baseMVA = opfdata.baseMVA
+    buses = opfdata.buses
+    lines = opfdata.lines
+    busIdx = opfdata.BusIdx
+    FromLines = opfdata.FromLines
+    ToLines = opfdata.ToLines
+    BusGeners = opfdata.BusGenerators
+    YffR,YffI,YttR,YttI,YftR,YftI,YtfR,YtfI,YshR,YshI = computeAdmitances(lines, buses, baseMVA)
+        
+
+    # Power Balance Equations
+    for b in 1:length(buses)
+        #real part
+        @NLconstraint(opfmodel, 
+            ( sum( YffR[l] for l in FromLines[b]) + sum( YttR[l] for l in ToLines[b]) + YshR[b] ) * Vm[b]^2 
+            + sum( Vm[b]*Vm[busIdx[lines[l].to]]  *( YftR[l]*cos(Va[b]-Va[busIdx[lines[l].to]]  ) + YftI[l]*sin(Va[b]-Va[busIdx[lines[l].to]]  )) for l in FromLines[b] )  
+            + sum( Vm[b]*Vm[busIdx[lines[l].from]]*( YtfR[l]*cos(Va[b]-Va[busIdx[lines[l].from]]) + YtfI[l]*sin(Va[b]-Va[busIdx[lines[l].from]])) for l in ToLines[b]   ) 
+            - ( sum(baseMVA*Pg[g] for g in BusGeners[b]) - Pd[b] + sigma_P1[b] - sigma_P2[b]) / baseMVA      # Sbus part
+            ==0
+        )
+    end
+end
+
+function add_imag_power_balance_constraints(opfmodel::JuMP.Model, opfdata::OPFData, Qg, Qd, Vm, Va, sigma_Q1, sigma_Q2)
+    # Network data short-hands
+    baseMVA = opfdata.baseMVA
+    buses = opfdata.buses
+    lines = opfdata.lines
+    busIdx = opfdata.BusIdx
+    FromLines = opfdata.FromLines
+    ToLines = opfdata.ToLines
+    BusGeners = opfdata.BusGenerators
+    YffR,YffI,YttR,YttI,YftR,YftI,YtfR,YtfI,YshR,YshI = computeAdmitances(lines, buses, baseMVA)
+        
+
+    # Power Balance Equations
+    for b in 1:length(buses)
+        #imaginary part
+        @NLconstraint(opfmodel,
+            ( sum(-YffI[l] for l in FromLines[b]) + sum(-YttI[l] for l in ToLines[b]) - YshI[b] ) * Vm[b]^2 
+            + sum( Vm[b]*Vm[busIdx[lines[l].to]]  *(-YftI[l]*cos(Va[b]-Va[busIdx[lines[l].to]]  ) + YftR[l]*sin(Va[b]-Va[busIdx[lines[l].to]]  )) for l in FromLines[b] )
+            + sum( Vm[b]*Vm[busIdx[lines[l].from]]*(-YtfI[l]*cos(Va[b]-Va[busIdx[lines[l].from]]) + YtfR[l]*sin(Va[b]-Va[busIdx[lines[l].from]])) for l in ToLines[b]   )
+            - ( sum(baseMVA*Qg[g] for g in BusGeners[b]) - Qd[b] + sigma_Q1[b] - sigma_Q2[b]) / baseMVA      #Sbus part
+            ==0
+        )
+    end
 end
