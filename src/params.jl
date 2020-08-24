4870f9f4d0171eccce0252bd214b7d67872d7b56
Anirudh Subramanyam <asubramanyam@moonshot.cels.anl.gov>
2020-08-24 14:47:02 -0500

    Move params to single file

--- src/params.jl
+++ src/params.jl
@@ -1,3 +1,74 @@
+#
+# Algorithmic parameters
+#
+mutable struct AlgParams
+    jacobi::Bool    # if true: do jacobi, else do gauss-siedel
+    iterlim::Int    # maximum number of iterations
+    tol::Float64    # tolerance used for termination
+    zero::Float64   # tolerance below which is regarded as zero
+    ρ::Any          # Augmented Lagrangian coefficient (can be different for different constraints)
+    τ::Float64      # Proximal coefficient
+    θ::Float64      # Relaxation parameter for update of dual variables
+    maxρ::Float64   # Maximum value of ρ for any constraint
+    updateρ::Bool   # Dynamically update ρ
+    updateτ::Bool   # (only for Proximal ALM) Dynamically update τ
+    nlpiterlim::Int # Maximum # of iterations in NLP step
+    mode::String    # computation mode (nondecomposed, coldstart, lyapunov_bound)
+
+    function AlgParams()
+        new(true,   # jacobi
+            100,    # iterlim
+            1e-4,   # tol
+            1e-8,   # zero
+            1.0,    # ρ
+            3.0,    # τ
+            1.0,    # θ
+            1.0,    # maxρ
+            false,  # updateρ
+            false,  # updateτ
+            1000,   # nlpiterlim
+            "nondecomposed" # mode
+        )
+    end
+end
+
+#
+# Model parameters and options
+#
+mutable struct ModelParams
+    num_time_periods::Int
+    num_ctgs::Int
+    obj_gencost::Bool
+    allow_constr_infeas::Bool
+    allow_load_shed::Bool
+    add_quadratic_penalty::Bool
+    weight_quadratic_penalty::Float64
+    weight_scencost::Float64
+    weight_loadshed::Float64
+    weight_freqctrl::Float64
+    savefile::String
+    ctgs_link_constr_type::String
+
+    function ModelParams()
+        new(1,     # num_time_periods
+            0,     # num_ctgs            
+            true,  # obj_gencost
+            false, # allow_constr_infeas
+            false, # allow_load_shed
+            false, # add_quadratic_penalty
+            1.0,   # weight_quadratic_penalty
+            1.0,   # weight_scencost
+            1.0,   # weight_loadshed
+            1.0,   # weight_freqctrl
+            "",    # savefile
+            "preventive" # ctgs_link_constr_type (preventive, corrective, frequency)
+        )
+    end
+end
+
+#
+# Algorithm data
+#
 mutable struct AlgNLPData
     nlpmodel::Vector{JuMP.Model}
     colCount::Int64
@@ -5,57 +76,5 @@ mutable struct AlgNLPData
     colLower::Array{Float64, 2}
     colUpper::Array{Float64, 2}
     colValue::Array{Float64, 2}
-end
-
-#
-# Algorithmic parameters
-#
-
-mutable struct AlgParams
-    aladin::Bool   # if true: do ALADIN, else do prox-ALM
-    jacobi::Bool   # (only for prox-ALM) if true: do jacobi, else do gauss-siedel
-    iterlim::Int   # maximum number of iterations
-    tol::Float64   # tolerance used for termination
-    zero::Float64  # tolerance below which is regarded as zero
-    ρ::Any         # Augmented Lagrangian coefficient (can be different for different constraints)
-    τ::Float64     # Proximal coefficient
-    μ::Float64     # (only for ALADIN) Augmented Lagrangian coefficient in QP
-    θ::Float64     # Relaxation parameter for update of dual variables
-    maxρ::Float64  # Maximum value of ρ for any constraint
-    updateρ::Bool  # Dynamically update ρ
-    updateτ::Bool  # (only for Proximal ALM) Dynamically update τ
-    nlpiterlim::Int# Maximum # of iterations in NLP step
-end
-
-function initializeParams(maxρ::Float64; aladin::Bool, jacobi::Bool, options::Option = Option())
-    iterlim = 100
-    tol = 1e-2
-    zero = 1e-4
-    ρ = maxρ
-    if aladin
-        τ = 0.0
-        μ = maxρ
-        nlpiterlim = 10000
-        updateρ = false
-    else
-        τ = 0
-        if jacobi && !options.two_block
-            τ = 3maxρ
-        end
-        μ = 0.0
-        nlpiterlim = 10000
-        # update AL parameter only if
-        # coupling constraints are inequalities
-        updateρ = false
-        if options.sc_constr
-            updateρ = !(options.freq_ctrl || options.two_block)
-        end
-        if options.has_ramping
-            updateρ = !options.quadratic_penalty
-        end
-    end
-    θ = 1.0
-    updateτ = false
-    return AlgParams(aladin, jacobi, iterlim, tol, zero, ρ, τ, μ, θ, maxρ, updateρ, updateτ, nlpiterlim)
 end
 
