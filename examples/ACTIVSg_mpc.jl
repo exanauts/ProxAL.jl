c40f11a93a1d91b0b0f4e619829659f489723c76
Anirudh Subramanyam <asubramanyam@moonshot.cels.anl.gov>
2022-04-05 00:32:15 -0500

    add mpc example

--- examples/ACTIVSg_mpc.jl
+++ examples/ACTIVSg_mpc.jl
@@ -16,7 +16,7 @@ solver     = length(ARGS) > 0 ? ARGS[1] : "exatron"
 num_sweeps = length(ARGS) > 1 ? parse(Int, ARGS[2]) : 2
 rho0       = length(ARGS) > 2 ? parse(Float64, ARGS[3]) : 1e-2
 obj_scale  = length(ARGS) > 3 ? parse(Float64, ARGS[4]) : 1e-3
-t_start    = length(ARGS) > 4 ? parse(Int, ARGS[5]) : 21
+T_init     = length(ARGS) > 4 ? parse(Int, ARGS[5]) : 21
 
 # choose case
 case = "case118"
@@ -29,11 +29,26 @@ backend = (solver == "ipopt") ? ProxAL.JuMPBackend() : ProxAL.ExaTronBackend()
 # case file
 case_file = joinpath(artifact"ExaData", "ExaData/matpower/$(case).m")
 
+
+# mpc setup
+Tsamples = 10
+x_warm = nothing
+λ_warm = nothing
+T = 12
+T_last = T_init + T + Tsamples - 1
+numiter = []
+wall_time_elapsed_actual = []
+wall_time_elapsed_ideal = []
+
+
+for (Tidx, Tcurrent) in enumerate(T_init:max(T_last-T+1,1))
+
+
 # Model/formulation settings
 modelinfo = ModelInfo()
 modelinfo.case_name = case
-modelinfo.time_horizon_start = t_start
-modelinfo.num_time_periods = 12
+modelinfo.time_horizon_start = Tcurrent
+modelinfo.num_time_periods = T
 modelinfo.load_scale = 1.0
 if startswith(case, "case_ACTIVSg")
     tfile = Int(24 * 7 * (60 / resolution))
@@ -76,14 +91,17 @@ end
 algparams.optimizer = optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0, "tol" => 1e-4) # "linear_solver" => "ma27"
 algparams.init_opf = false
 
+
 # dry run to compile everything
-algparams.verbose = 0
-algparams.iterlim = 1
-redirect_stdout(devnull) do
-    global elapsed_t_dry_run_create_problem = @elapsed begin
-        nlp = ProxALEvaluator(case_file, load_file, modelinfo, algparams, backend)
+if Tidx == 1
+    algparams.verbose = 0
+    algparams.iterlim = 1
+    redirect_stdout(devnull) do
+        global elapsed_t_dry_run_create_problem = @elapsed begin
+            nlp = ProxALEvaluator(case_file, load_file, modelinfo, algparams, backend)
+        end
+        global elapsed_t_dry_run_optimize = @elapsed ProxAL.optimize!(nlp)
     end
-    global elapsed_t_dry_run_optimize = @elapsed ProxAL.optimize!(nlp)
 end
 
 # Set up and solve problem
@@ -97,6 +115,32 @@ elapsed_t = @elapsed begin
         global nlp = ProxALEvaluator(case_file, load_file, modelinfo, algparams, backend)
     end
 end
+
+
+if Tidx > 1
+    for t=1:(T-1)
+        @views begin
+            x_warm.Pg[:,:,t] .= x_warm.Pg[:,:,t+1]
+            x_warm.Qg[:,:,t] .= x_warm.Qg[:,:,t+1]
+            x_warm.Vm[:,:,t] .= x_warm.Vm[:,:,t+1]
+            x_warm.Va[:,:,t] .= x_warm.Va[:,:,t+1]
+            x_warm.ωt[:,t] .= x_warm.ωt[:,t+1]
+            x_warm.St[:,t] .= x_warm.St[:,t+1]
+            x_warm.Zt[:,t] .= x_warm.Zt[:,t+1]
+            x_warm.Sk[:,:,t] .= x_warm.Sk[:,:,t+1]
+            x_warm.Zk[:,:,t] .= x_warm.Zk[:,:,t+1]
+            x_warm.sigma[:,t] .= x_warm.sigma[:,t+1]
+            λ_warm.ramping[:,t] .= λ_warm.ramping[:,t+1]
+            λ_warm.ctgs[:,:,t] .= λ_warm.ctgs[:,:,t+1]
+        end
+    end
+
+    nlp.problem.x = deepcopy(x_warm)
+    nlp.problem.λ = deepcopy(λ_warm)
+end
+
+
+
 if MPI.Comm_rank(MPI.COMM_WORLD) == 0
     global_logger(cur_logger)
     println("Creating problem: $elapsed_t")
@@ -106,6 +150,7 @@ if MPI.Comm_rank(MPI.COMM_WORLD) == 0
     end
 
     @show(ARGS)
+    @show(Tcurrent)
     @show(info.iter)
     @show(info.maxviol_d)
     @show(info.maxviol_t_actual)
@@ -114,6 +159,20 @@ if MPI.Comm_rank(MPI.COMM_WORLD) == 0
     @show(info.wall_time_elapsed_ideal)
 else
     info = ProxAL.optimize!(nlp; ρ_t_initial = rho0, τ_factor = 2.5)
+end
+
+global x_warm = deepcopy(nlp.problem.x)
+global λ_warm = deepcopy(nlp.problem.λ)
+
+push!(numiter, info.iter)
+push!(wall_time_elapsed_actual, info.wall_time_elapsed_actual)
+push!(wall_time_elapsed_ideal, info.wall_time_elapsed_ideal)
+end
+
+if MPI.Comm_rank(MPI.COMM_WORLD) == 0
+    @show(numiter)
+    @show(wall_time_elapsed_actual)
+    @show(wall_time_elapsed_ideal)
 end
 
 MPI.Finalize()
