374144a84614c14e3b31ddd49feb9d71e0b2148c
Michel Schanen <mschanen@anl.gov>
2022-09-19 12:46:24 -0500

    Fixes and bumps
    * Fix hdf5 on moonshot
    * Bump ExaAdmm version enabling portability on the latest CUDA.jl and
      AMDGPU.jl

--- setup_moonshot.sh
+++ setup_moonshot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-module load intel mpich hdf5
-julia --project -e 'ENV["JULIA_HDF5_PATH"]="/nfs/gce/software/custom/linux-ubuntu18.04-x86_64/hdf5/1.12.1-mpich-3.4.2-intel-parallel-fortran"; ENV["JULIA_MPI_BINARY"]="system"; ENV["JULIA_MPI_PATH"]="/nfs/gce/software/custom/linux-ubuntu18.04-x86_64/mpich/3.4.2-intel"; using Pkg; Pkg.build("MPI"; verbose=true); Pkg.build("HDF5"; verbose=true)'
+export JULIA_HDF5_PATH="/disk/hdf5/hdf5-1.12.2/build/bin"
+export JULIA_MPI_BINARY="system"
+julia --project -e 'using Pkg; Pkg.build("MPI"; verbose=true); Pkg.build("HDF5"; verbose=true)'
 julia --project -e 'using MPI ; MPI.install_mpiexecjl(force=true ;destdir=".")'
